25436d6de6aaa35f2166e35d86b5b4dc622bb17c
KarelVanek359 <167568748+KarelVanek359@users.noreply.github.com>
2024-04-20 21:14:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 import streamlit as st
-import webbrowser
 st.markdown("# Kabinet fyziky GVHL")
 
 st.image("gvh_logo.jpg")
-st.markdown("### Kliknutím na tlačítko přejdeš na stránku se soubory:")
+st.markdown("### Kliknutím na odkazu přejdeš na stránku se soubory:")
 
-def open_url(url):
-    webbrowser.open_new_tab(url)
-tlacitko=st.button("Přejdi na cloud",on_click=open_url("https://onedrive.live.com/?authkey=%21AAdbH59Zolh4Vcg&id=815B93FBD69EACCA%21106&cid=815B93FBD69EACCA"))
+st.markdown("[Klikni zde!](https://onedrive.live.com/?authkey=%21AAdbH59Zolh4Vcg&id=815B93FBD69EACCA%21106&cid=815B93FBD69EACCA)")
 
 
